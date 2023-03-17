bd9d0be5c978612b83f41ae59076c43178be4a23
Great <greatchukwumah@gmail.com>
2023-03-17 01:55:24 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+
+/*
+ * main - this is a main function
+ *
+ * Return: always 0
+ */
+
 int main(void)
-{	
-	char hello [] = "Programming is like building a multilingual puzzle" ;
-	puts(hello) ;
-	return 0
+{
+	char hello[] = "Programming is like building a multilingual puzzle";
+
+	puts(hello);
+	return (0);
 }
-
-
-
