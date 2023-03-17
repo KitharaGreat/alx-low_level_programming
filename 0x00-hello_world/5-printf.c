27530a12f753a5553b7eee03b14e877274d06940
Great <greatchukwumah@gmail.com>
2023-03-17 02:29:59 +0000

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
-#include<stdio>
+#include<stdio.h>
 /**
  *main: The purpose of main's return value is to return an exit status to the operating system.
  *Return: this function returns it
  */
-int main(void);
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,/n");
 
