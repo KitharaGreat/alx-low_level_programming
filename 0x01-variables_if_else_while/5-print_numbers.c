156847e33fbedfeac8e6cce98994d58f31981e97
Great <greatchukwumah@gmail.com>
2023-03-18 03:06:14 +0000

    printnum

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - this will print all single digits
+ *
+ *Return: will return 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0 ; a >= 9 ; a++)
+		printf("%d \n", a);
+	return (0);
+}
