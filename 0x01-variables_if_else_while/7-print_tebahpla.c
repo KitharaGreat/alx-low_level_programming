0a9edcc676f74aae509ea9b0706ecde4357ed12b
Great <greatchukwumah@gmail.com>
2023-03-18 12:38:54 +0000

    ddd

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - reverse alpha
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'z' ; i <= 'a' ; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
