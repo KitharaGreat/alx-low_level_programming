92ec8628ac9c01f16e3b904de320d7f2aeeabb20
Great <greatchukwumah@gmail.com>
2023-03-18 02:16:11 +0000

    for loop alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - to print alphabets in lowercase
+ *
+ * Return: it returns 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar (i);
+	putchar ('\n');
+	return (0);
+
+}
