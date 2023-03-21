f3e77c81e48fde31dba904b29907d5a0bd6a35a4
Great <greatchukwumah@gmail.com>
2023-03-21 21:05:41 +0000

    is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isalpha: function to know if its an alpha
+ *
+ * @c: input to be evaluated
+ *
+ * Return: 1 if success 0 if failure
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+		return (1);
+	else
+		return(0);
+}
