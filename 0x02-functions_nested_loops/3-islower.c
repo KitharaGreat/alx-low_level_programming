48aaf4830e4a6bbbc250c5976e865dfe3b2beb11
Great <greatchukwumah@gmail.com>
2023-03-21 19:45:57 +0000

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *  _islower - function to find if lower case
+ *
+ * @c: parameter to be evaluated
+ *
+ * Return: 1 if successful 0 if not
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c >= 122)
+		return (1);
+	else
+		return (0);
+}
