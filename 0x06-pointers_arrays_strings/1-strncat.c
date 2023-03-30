8a61e492fdedc1b74e1df974f36331ab67c9e2ac
Great <greatchukwumah@gmail.com>
2023-03-30 21:37:18 +0000

    ll

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcat - concatenate 2 strings
+ *
+ * @dest: parameter to be joined to
+ * @n: number of char to be added
+ * @src: the the one to be added
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+        int countdest = 0;
+        int countsrc = 0;
+        int a;
+
+        for (a = 0 ; dest[a] != '\0'; a++)
+                countdest++;
+        for (a = 0 ; src[a] != '\0' ; a++)
+                countsrc++;
+        for (a = 0 ; a <= n ; a++)
+                dest[countdest + a] = src[a];
+        return (dest);
+}
